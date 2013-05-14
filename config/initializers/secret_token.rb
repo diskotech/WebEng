a4670280b24557ad2fcb58cb0ebd23309799de3d
Konstantina <kgemenet@student.ethz.ch>
2013-05-15 01:03:09 +0200

    Created a rails app and added status and trip updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebEng::Application.config.secret_token = '2adf1dff0c2294a6cd7b351f1ee9cf6ad0fc33d2a1def0e522d272e568e7de0ed92495b8d1b94f6c52f617f76a09276bc80b62d9ce590c497a92aa51ea9512dd'
